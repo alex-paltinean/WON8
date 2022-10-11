9ef57dd2c971121b8a51f1c5a4a0c3d2cae0c603
Alex Paltinean <alex.paltinean@appointfix.com>
2022-10-11 21:29:56 +0300

    curs12

---
+++ src/org/fasttrackit/exceptions/NumberTooLargeException.java
@@ -0,0 +1,18 @@
+package org.fasttrackit.exceptions;
+
+public class NumberTooLargeException extends Exception {
+    private int number;
+
+    public NumberTooLargeException(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public String getMessage() {
+        return String.format("Number %d is too large", number);
+    }
+}
---
+++ src/org/fasttrackit/exceptions/PowerTooLargeException.java
@@ -0,0 +1,7 @@
+package org.fasttrackit.exceptions;
+
+public class PowerTooLargeException extends Exception {
+    public PowerTooLargeException(String message) {
+        super(message);
+    }
+}
